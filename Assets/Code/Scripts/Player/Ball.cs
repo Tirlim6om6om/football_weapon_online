c30a8a8e24ddb71f7b8902db8f5914164a312b63
Tirlim6om6om <57911899+Tirlim6om6om@users.noreply.github.com>
2024-01-25 22:44:07 +0300

    Прототипная версия системы стрельбы

---
+++ Assets/Code/Scripts/Player/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Scripts.Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Ball : Shell
+    {
+        [SerializeField] private float multiplyForce;
+        private Rigidbody _rb;
+
+        private void Awake()
+        {
+            TryGetComponent(out _rb);
+        }
+
+        public override void SetPhysics(bool active)
+        {
+            _rb.isKinematic = !active;
+        }
+
+        public override void Kick(Vector3 axis)
+        {
+            _rb.AddForce(axis * multiplyForce);
+        }
+    }
+}
